5c4f42074b7c60fff889aea2623809f7ac8f7805
Miguel Castillo <miguel.castillo@ibit.pe>
2025-01-25 11:43:54 -0500

    feat: agregando variables de entorno, registro y login para el usuario

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './strategies/local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    ConfigModule, // Importa ConfigModule para usar ConfigService
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule], // Necesario para usar ConfigService
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRATION') || '1h', // Valor por defecto en caso de no estar definido
+        },
+      }),
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-/*
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
@@ -12,5 +12,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       secretOrKey: 'asdfsaf',
     });
   }
+
+  async validate(payload) {
+    return { userId: payload.sub, username: payload.username }
+  }
 }
-*/
+
