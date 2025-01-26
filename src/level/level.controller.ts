a713a8a949177ee8ec0ae2a91d0462b1567ca3df
Miguel Castillo <miguel.castillo@ibit.pe>
2025-01-26 16:56:38 -0500

    feat: agregando endpoint para enviar los niveles

--- src/level/level.controller.ts
+++ src/level/level.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { LevelService } from './level.service';
+import { Level } from './entities/level.entity';
 
 @Controller('level')
 export class LevelController {
   constructor(private readonly levelService: LevelService) {}
+
+  @Get()
+  async findAll(): Promise<Level[]> {
+    return this.levelService.findAll()
+  }
 }
