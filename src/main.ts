abe9d6abe045ccfcff51104377c4bbcdc6652bb7
Miguel Castillo <miguel.castillo@ibit.pe>
2025-01-26 22:19:50 -0500

    feat: agregando configuración de CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOrigin = process.env.CORS_ORIGIN || '*'; // Usa '*' si no está definida la variable
+
+  app.enableCors({
+    origin: corsOrigin, // Permite el origen específico
+    methods: 'GET, POST, PUT', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
+    credentials: true, // Si necesitas enviar cookies o cabeceras de autenticación
+  });
 
   // Configuración básica de Swagger
   const config = new DocumentBuilder()
